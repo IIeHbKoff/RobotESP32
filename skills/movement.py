77d33c2015010ce9bf9ebc3530a259b988c049e2
AleksandrBelyan <belyan.alex93@gmail.com>
2022-12-03 14:19:02 +0300

    Refactored movement.py and add speed to funcs

--- skills/movement.py
+++ skills/movement.py
@@ -1,40 +1,21 @@
-from machine import PWM
-from config import Config
+from libs import MX1508
 
 
 class MovementSkill:
     def __init__(self):
-        self.pwm1_a = PWM(Config.motor1_a, freq=Config.motors_freq)
-        self.pwm1_b = PWM(Config.motor1_b, freq=Config.motors_freq)
-        self.pwm2_a = PWM(Config.motor2_a, freq=Config.motors_freq)
-        self.pwm2_b = PWM(Config.motor2_b, freq=Config.motors_freq)
+        self.driver = MX1508()
 
-    def move_forward(self, speed: int):
-        self.pwm1_a.duty_u16(30000)
-        self.pwm1_b.duty_u16(0)
-        self.pwm2_a.duty_u16(30000)
-        self.pwm2_b.duty_u16(0)
+    def move_forward(self, speed: int) -> None:
+        self.driver.move_forward(speed=speed)
 
-    def move_backward(self, speed: int):
-        self.pwm1_a.duty_u16(0)
-        self.pwm1_b.duty_u16(30000)
-        self.pwm2_a.duty_u16(0)
-        self.pwm2_b.duty_u16(30000)
+    def move_backward(self, speed: int) -> None:
+        self.driver.move_backward(speed=speed)
 
-    def move_left(self, speed: int):
-        self.pwm1_a.duty_u16(30000)
-        self.pwm1_b.duty_u16(0)
-        self.pwm2_a.duty_u16(0)
-        self.pwm2_b.duty_u16(30000)
+    def move_left(self, speed: int) -> None:
+        self.driver.move_left(speed=speed)
 
-    def move_right(self, speed: int):
-        self.pwm1_a.duty_u16(0)
-        self.pwm1_b.duty_u16(30000)
-        self.pwm2_a.duty_u16(30000)
-        self.pwm2_b.duty_u16(0)
+    def move_right(self, speed: int) -> None:
+        self.driver.move_right(speed=speed)
 
-    def stop(self):
-        self.pwm1_a.duty_u16(0)
-        self.pwm1_b.duty_u16(0)
-        self.pwm2_a.duty_u16(0)
-        self.pwm2_b.duty_u16(0)
+    def stop(self) -> None:
+        self.driver.stop()
